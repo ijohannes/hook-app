1dde5c517e0aded793c7395d36791ea09238f668
Nestor Triana <ngtriana12@gmail.com>
2023-11-02 19:55:08 -0500

    formulario básico con useEffect

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HookApp } from './HookApp';
+// import { SimpleForm } from './components/02-useEffect/SimpleForm';
+import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
-    <HookApp />
+    <FormWithCustomHook />
 
 );
 
